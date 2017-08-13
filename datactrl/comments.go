0694aba8ae4408c02c54522822a287a6dcc8b0de
Steve Wood <emadera52@hotmail.com>
2017-08-13 18:33:17 -0500

    Add files via upload

---
+++ datactrl/comments.go
@@ -0,0 +1,68 @@
+package datactrl
+
+import (
+	// Standard library
+	"strconv"
+	"time"
+	
+	// Internal
+	"mvsf/models"
+)
+
+// Types and method definitions used to add methods to models.CmntData
+
+type roCmntData struct {
+	ModelCD models.CmntData
+}
+
+// This is possible thanks to powers granted to an empty struct 
+type roCD struct{}
+
+// roComment allows us to effectively add methods to models.CmntData. All data
+// and "local" methods, if any, defined for models.CmntData are accessible.
+type roComment struct {
+	*roCmntData
+	*roCD
+}
+
+// Other packages can use Rcd to access methods added to models.CmntData
+var	Rcd = roComment {
+		roCmntData: &roCmntData{},
+		roCD: &roCD{},
+	}
+	
+// Remote methods called by "meth" test
+func (cd *roComment) NewComment(ncd models.CmntData) *models.CmntData {
+
+	cd.roCmntData.ModelCD = ncd
+	cd.ModelCD.DateCreated = time.Now()
+	
+	return &cd.ModelCD
+}
+
+func (cd *roComment) DefaultComment(cnt int) *models.CmntData {
+
+	dcd := models.DefaultCD
+	dcd.Slug = "demo-comment-" + strconv.Itoa(cnt + 1)
+	
+	return cd.NewComment(dcd)
+}
+
+
+// Remote functions called by "func" test
+
+func NewComment(cd models.CmntData) *models.CmntData {
+
+	cd.DateCreated = time.Now()
+
+	return &cd
+}
+
+func DefaultComment(cnt int) *models.CmntData {
+
+	cd := models.DefaultCD 
+	cd.Slug = "demo-comment-" + strconv.Itoa(cnt + 1)
+
+	return NewComment(cd)
+}
+
---
+++ models/models.go
@@ -0,0 +1,58 @@
+package models
+
+import (
+	// Standard library
+	"strconv"
+	"time"
+	
+)
+
+type CmntData struct {
+	ID          int
+	Slug        string
+	Title       string
+	PageID      int
+	Text		string
+	DateCreated time.Time
+	CreatedBy   int
+}
+
+var DefaultCD = CmntData{ID: 100, Title: "Demo Comment",
+	Text: "This is a test comment", PageID: 1000, CreatedBy: 4242}
+	
+
+// Local methods called by "method" test
+	
+func (cd *CmntData) NewComment(ncd CmntData) *CmntData {
+
+	ncd.DateCreated = time.Now()
+	
+	return &ncd
+}
+
+func (cd *CmntData) DefaultComment(cnt int) *CmntData {
+
+	dcd := DefaultCD
+	dcd.Slug = "demo-comment-" + strconv.Itoa(cnt + 1)
+	
+	return cd.NewComment(dcd)
+}
+
+
+// Local functions called by "function" test
+
+func NewComment(cd CmntData) *CmntData {
+
+	cd.DateCreated = time.Now()
+
+	return &cd
+}
+
+func DefaultComment(cnt int) *CmntData {
+
+	cd := DefaultCD 
+	cd.Slug = "demo-comment-" + strconv.Itoa(cnt + 1)
+
+	return NewComment(cd)
+}
+
